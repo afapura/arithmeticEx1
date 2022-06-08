dc16b4bfdfd05e22a5ebcb0483041e52044f0177
afapura <aluno81664@ipt.pt>
2022-06-09 00:10:19 +0100

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+
+from setuptools import setup, find_packages
+setup(
+    name="arithmetic",
+    version="0.2",
+    description="Funcoes Aritmeticas",
+    packages=find_packages(),
+    include_package_data=True
+)
